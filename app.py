72de74c9409a71b47131fbcad8d5afbcc380beb3
Robby Renz <nxgrenz@gmail.com>
2019-10-04 18:09:50 -0500

    add list of regions

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import requests
+import random
+
+app = Flask(__name__)
+
+regions = ['australia', 'canada', 'denmark', 'england', 'ireland', 'sweden', 'switzerland', 'united states']
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
