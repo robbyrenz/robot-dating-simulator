6a72959cc3d2de3543497a9afebdf46d6997e36d
Robby Renz <nxgrenz@gmail.com>
2019-10-06 14:09:24 -0500

    add code that accurately makes a random user with an avatar

--- user_generation.py
+++ user_generation.py
@@ -24,29 +24,72 @@ race = {
 
 def main():
     region = random_region(regions)
-    race = random_race(race)
+    user = generate_users_and_photo(region)
+    print(user)
 
 
-def generate_users_and_photo(region: str, ROBOHASH_URL: str):
+def generate_users_and_photo(region: str):
     """
+    Generates users and avatars from two different APIs.
+
     :param region: a string representing a region
-    :param race: a string representing a race of the person
+    :return: returns a list of dictionaries
     """
+
     res = requests.get("https://uinames.com/api", params={"region": region})
     res = res.json()
 
+    full_name = res['name'] + " " + res['surname']
+    print("Full name:", full_name)
 
-def random_region(list: list):
-    region = random.choice(list)
+    species = random_race(race)
+    print("Species:", species)
+
+    # get the url from the name and species
+    url = robohash_url_generator(full_name, species)
+
+    # now add the url to the dictionary
+    res['avatar'] = url
+    return res
+
+
+def random_region(regions: list):
+    """
+    Picks a random item (a region) from a list.
+
+    :param regions: a list
+    :return: returns a string
+    """
+
+    region = random.choice(regions)
     return region
 
 
 def random_race(race: dict):
-    race = random.choice(race.values())
-    return race
+    """
+    Chooses a random value from a dictionary
+
+    :param: race: a dictionary
+    :return: returns a string (the value of the chosen key)
+    """
+
+    # race = random.choice(race.values()) NOT WORKING!!! I'm getting -> TypeError: 'dict_values' object is not subscriptable
+    list_of_species = []
+    for i in race.values():
+        list_of_species.append(i)
+    # print(list_of_species)
+    return random.choice(list_of_species)
 
 
 def robohash_url_generator(name: str, race: str):
+    """
+    Generates a URL from the given parameters. The URL represents an image.
+
+    :param: name: the name of the person
+    :param: race: the race of the person
+    :return: a string/url representing an image
+    """
+
     ROBOHASH_URL =  f"https://robohash.org/{name}.png?set={race}"
     return ROBOHASH_URL
 
